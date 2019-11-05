7f996a0a50495e626e8a98e5f9939e04903807fc
pratik <pratik.sathe1987@gmail.com>
2019-11-05 14:19:21 +0530

    Setup basic stock component layout and passing props to Stock component from Stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,25 @@
 <template>
-  <h3>
-    The Stocks component
-  </h3>
+  <div>
+    <app-stock v-for="(stock, ind) in stocks" :key="ind" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'Google', price: 340 },
+          { id: 3, name: 'Apple', price: 400 },
+          { id: 4, name: 'Twitter', price: 9 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
