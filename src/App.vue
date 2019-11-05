dbde1d514341f45f90cdf37dcfaf81df83e72cb2
pratik <pratik.sathe1987@gmail.com>
2019-11-05 17:22:50 +0530

    Added animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <div class="row">
       <div class="col-xs-12">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -25,5 +27,35 @@
 <style>
   body {
     padding: 30px;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translate(-30);
+      opacity: 0;
+    }
+    to {
+      transform: translate(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translate(0);
+      opacity: 1;
+    }
+    to {
+      transform: translate(-30);
+      opacity: 0;
+    }
   }
 </style>
