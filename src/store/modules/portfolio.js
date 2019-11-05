a82c5b60c5510cd00f20dbcd22c81bbfe2a03d78
pratik <pratik.sathe1987@gmail.com>
2019-11-05 15:53:48 +0530

    Added portfolio module

---
+++ src/store/modules/portfolio.js
@@ -0,0 +1,60 @@
+const state = {
+  funds: 10000,
+  stocks: []
+}
+
+const mutations = {
+  'BUY_STOCK'(state, { stockId, stockPrice, quantity}) {
+    const record = state.stocks.find(stock => stock.id == stockId);
+    console.log('Quantity', quantity, stockId, stockPrice, record)
+    if (record) {
+      record.quantity += quantity;
+    } else {
+      state.stocks.push({
+        id: stockId,
+        quantity: quantity
+      });
+    }
+    state.funds -= quantity * stockPrice;
+  },
+  'SELL_STOCK'(state, { stockId, stockPrice, quantity}) {
+    const record = state.stocks.find(stock => stockId == stock.id);
+    if (record.quantity > quantity) {
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(record, 1);
+    }
+
+    state.funds += quantity * stockPrice;
+  }
+}
+
+const actions = {
+  'sellStock'({commit}, order) {
+    commit('SELL_STOCK', order);
+  }
+}
+
+const getters = {
+  stockPortfolio(state, getters) {
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(element => element.id === stock.id);
+      return {
+        id: stock.id,
+        quantity: stock.quantity,
+        price: record.price,
+        name: record.name
+      }
+    })
+  },
+  funds(state) {
+    return state.funds;
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
