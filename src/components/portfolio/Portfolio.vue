c45f680b0336a530cdd3dfc0fe08f2c801e9f5a1
pratik <pratik.sathe1987@gmail.com>
2019-11-05 15:56:24 +0530

    Added portfolio - stock component implementation

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,5 +1,21 @@
 <template>
-  <h3>
-    The Portfolio component
-  </h3>
+  <div>
+    <app-stock v-for="(stock, ind) in stocks" :stock="stock" :key="ind"></app-stock>
+  </div>
 </template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Stock from './Stock.vue';
+
+  export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio'
+      })
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
