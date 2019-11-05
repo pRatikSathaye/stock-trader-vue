0eabe37671c9338e03b5a38c340cada8a3b7b74d
pratik <pratik.sathe1987@gmail.com>
2019-11-05 17:22:38 +0530

    Added dropdown functionality and randomizeStock implementation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,8 +12,8 @@
         </ul>
         <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">End Day</a></li>
-          <li class="dropdown">
+          <li><a href="#" @click="endDay">End Day</a></li>
+          <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
             <a 
               href="#"
               class="dropdown-toggle"
@@ -33,10 +33,24 @@
 </template>
 
 <script>
+  import { mapActions } from 'vuex';
   export default {
+    data() {
+      return {
+        isDropdownOpen: false
+      }
+    },
     computed: {
       funds() {
         return this.$store.getters.funds;
+      }
+    },
+    methods: {
+      ...mapActions([
+        'randomizeStocks'
+      ]),
+      endDay() {
+        this.randomizeStocks();
       }
     }
   }
